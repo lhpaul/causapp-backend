b5b50100480a22f571c473cf50e7484f4753f89e
Luis Hernan Paul Ossandon <lhpaul11@gmail.com>
2015-05-22 11:46:22 -0300

    haciendo que funcione con Beanstalk

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ import json
 import threading
 from funciones_scrap import *
 
-app = Flask(__name__)
-app.debug=True
+application = Flask(__name__)
+application.debug=True
 cookies = {}
 
 def reload_cookies():
@@ -14,25 +14,25 @@ def reload_cookies():
   threading.Timer(300.0, reload_cookies).start()
   cookies=get_cookies('http://civil.poderjudicial.cl/CIVILPORWEB/')
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/rut/<string:rut>')
+@application.route('/rut/<string:rut>')
 def get_info(rut):
   global cookies
   dv = rut[-1]
   rutnum = rut[:-1]
   # resultados=rut_scraper(rutnum,dv,cookies)
   array = [{'rol': 'CT556644', 'algo': 'algo'}, {'rol': 'CT553344', 'algo': 'algo'}]
-  return Response(json.dumps(array),  mimetype='application/json')
+  return Response(json.dumps(array),  mimetype='applicationlication/json')
 
-@app.route('/cookies')
+@application.route('/cookies')
 def cookies():
   
   return get_cookies('http://civil.poderjudicial.cl/CIVILPORWEB/')
 
 if __name__ == '__main__':
   # reload_cookies()
-  # app.run(debug=True)
-  app.run(host='0.0.0.0')
+  # application.run(debug=True)
+  application.run(host='0.0.0.0')
